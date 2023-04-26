0e919cacdd8f6580a1537eaa63fd18e7ee2c729b
Jyoti28490 <Jyotirmayi.swain@gmail.com>
2023-04-26 20:45:29 +0200

    second commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="bg-white shadow h-16 flex justify-between items-stretch">
+    <router-link
+      :to="{ name: 'home' }"
+      class="inline-flex items-center h-full px-5"
+    >
+      Home
+    </router-link>
+    <div>
+      <router-link
+        :to="{ name: 'byName' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-200"
+      >
+        Search Meals
+      </router-link>
+
+      <router-link
+        :to="{ name: 'byLetter' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-200"
+      >
+        Meals By Letter
+      </router-link>
+
+      <router-link
+        :to="{ name: 'ingredients' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-200"
+      >
+        Meals By Ingredients
+      </router-link>
+    </div>
+  </header>
+</template>
